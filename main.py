fbbb705535ccda825f6ac18e16b843d4626c2255
Carlos Ardila <carlos.ardila@appgate.com>
2022-02-18 16:54:59 -0500

    First Solution

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    INPUT_FILE = "inputs/03_BinaryDiagnostics.txt"
+    with open(INPUT_FILE) as f:
+        diagnosticfile = [binary_string for line in f if (binary_string := line.strip())]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    gamma = ''
+    epsilon = ''
+    num = len(diagnosticfile)
+
+    for i in range(len(diagnosticfile[0])):
+        num_ones = sum(int(diagnosticfile[j][i]) for j in range(num))
+        if num_ones >= (num - num_ones):
+            epsilon += "1"
+            gamma += "0"
+        else:
+            epsilon += "0"
+            gamma += "1"
+
+    print("Binary Diagnostics - Part 1 : ", int(epsilon, 2) * int(gamma, 2))
